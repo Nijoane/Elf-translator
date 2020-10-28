6eaff69b77b4552b0fec3e4320393c3ac4d8b281
Nijoane <nicajoane@outlook.com>
2020-10-27 23:44:34 -0300

    construindo lógica do código

--- cipher.js
+++ cipher.js
@@ -1,5 +1,39 @@
 const cipher = {
-  // ...
-};
+  encode: (str, offset) => {
+    let thisCipher = "";
+    for(let i = 0; i < str.length; i++) {
+      let code = str.charCodeAt(i);
+
+      if(code >= 65 && code <= 90) {
+        thisCipher += String.fromCharCode(((code -65 + offset)%26)+ 65);
+
+      } else if(code >= 97 && code <= 122) {
+        thisCipher += String.fromCharCode(((code -97 + offset)%26)+ 97);
+
+      } else {
+        thisCipher += str.charAt(i);
+      }
+    }  
+    return thisCipher;
+  },
+
+  decode: (str, offset) => {
+    let thisDecipher = "";
+    for(let i = 0; i < str.length; i++) {
+      let code = str.charCodeAt(i);
+
+      if(code >= 65 && code <= 90) {
+        thisDecipher += String.fromCharCode((code - 90 - offset)%26 + 90);
+
+      } else if(code >= 97 && code <= 122) {
+        thisDecipher += String.fromCharCode((code - 122 - offset)%26  + 122);
+
+      } else {
+        thisDecipher += str.charAt(i);
+      }
+    }  
+    return thisDecipher;
+  },
+}
 
 export default cipher;
