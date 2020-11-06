f7a98f400ff1f1a20ab77258dc5aaedfde3dbde8
Nijoane <nicajoane@outlook.com>
2020-11-05 22:50:37 -0300

    finalizando DOM e adicionando preventDefault

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ import cipher from './cipher.js';
 const btnEncodeElement = document.getElementById("btnEncode");
 function encodeMessage(event) {
   event.preventDefault();
-
   let str = document.getElementById("str").value;
 
   const offset = Number(document.getElementById("offset").value);
   const messageEncode = cipher.encode(str, offset);
 
-  document.getElementById("result").innerHTML = messageEncode;
+  document.getElementById("result").value = messageEncode;
 }
 
 btnEncodeElement.addEventListener('click', encodeMessage);
@@ -17,16 +16,12 @@ btnEncodeElement.addEventListener('click', encodeMessage);
 const btnDecodeElement = document.getElementById("btnDecode");
 function decodeMessage(event) {
   event.preventDefault();
-
   let str = document.getElementById("str").value;
 
   const offset = Number(document.getElementById("offset").value);
   const messageDecode = cipher.decode(str, offset);
   
- document.getElementById("result").innerHTML = messageDecode;
+ document.getElementById("result").value = messageDecode;
 }
 
 btnDecodeElement.addEventListener('click', decodeMessage);
-
-// eslint-disable-next-line no-console
-console.log(cipher);
